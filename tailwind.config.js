c94d46a4eb7d5dded84f1e741ff8ef77a8f534dc
Steib-Audy-Victoria <95017413+Steib-Audy-Victoria@users.noreply.github.com>
2022-12-28 15:56:18 +0100

    config tailwind

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,28 @@ module.exports = {
     "./src/**/*.{vue,js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        'Pink01': '#FACEB9',
+        'Jaune': '#EFC270',
+        'Pink02': '#F2836B',
+        'Rouge': '#CB2E48',
+        'Orange': '#F24405',
+        'Grey01': '#1B1B1B',
+        'Grey02': '#2C2C2C',
+        'Grey03': '#F2F2F2',
+        'Blue01': '#031059',
+        'Blue02': '#2C3D73',
+        'Blue04': '#415EB7',
+        'Blue05': '#3E95EF',
+        'Gris': '#EFEFEF',
+        'GrisF': '#2C2C2C',
+
+      },
+      fontFamily: {
+        "Montserrat": ['Montserrat', 'sans-serif'],
+      },
+    },
   },
   plugins: [
     require('prettier-plugin-tailwindcss'),
