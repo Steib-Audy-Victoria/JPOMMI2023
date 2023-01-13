0ec879d6ce8256ed905f1e6b37a2ce935acf6889
Steib-Audy-Victoria <95017413+Steib-Audy-Victoria@users.noreply.github.com>
2023-01-13 15:18:58 +0100

    hover btn connexion

--- src/components/btnConnexion.vue
+++ src/components/btnConnexion.vue
@@ -1,5 +1,5 @@
 <template>
-    <div class="p-4 bg-Rouge rounded-lg w-fit">
+    <div class="p-4 bg-Rouge hover:bg-Blue02 rounded-lg w-fit">
         <p class="text-white text-lg text-center font-Montserrat font-medium  md:text-xl">INSCRIPTION / CONNEXION</p>
     </div>
 </template>
