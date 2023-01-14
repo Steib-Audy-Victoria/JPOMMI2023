0b17ba51f010029f1bf9b8b4644dec0506ddc318
Steib-Audy-Victoria <95017413+Steib-Audy-Victoria@users.noreply.github.com>
2023-01-14 11:54:33 +0100

    page mentions légales a finir

---
+++ src/views/MentionLegalView.vue
@@ -0,0 +1,82 @@
+<template>
+    <div class="flex justify-center items-center py-12">
+        <h1 class="text-center text-xl md:text-2xl text-Blue01 font-semibold z-10">MENTIONS LÉGALES</h1>
+        <div class="w-16 h-5 bg-Jaune -translate-x-11 translate-y-2 z-0"></div>
+    </div>
+
+    <div class="p-10">
+        <p>Cette page décrit les mentions légales qui s'appliquent à tout internaute visitant ce site. En le consultant,
+            vous vous
+            engagez sans réserve à les respecter. Les mentions légales pouvant être modifiées à tout moment et sans
+            préavis, nous
+            vous engageons à les consulter régulièrement.</p>
+
+        <div class="my-4">
+            <p>Responsable éditorial et responsable légal :</p>
+            <p>"Nom de la structure"</p>
+            <p>Représenté(e) par :</p>
+            <p>Adresse :</p>
+            <p>E-mail :</p>
+            <p>Siret ou N°TVA :</p>
+        </div>
+
+        <div class="my-4">
+            <p>Éditeur du site :</p>
+            <p>Lavaine Iris, Monturet Matthieu, Paquier Thomas, Steib–audy Victoria</p>
+            <p>Représenté(e) par : Alain Lamboux-Durand</p>
+            <p>E-mail : jpo.mmi2023@gmail.com</p>
+        </div>
+
+        <div class="my-4">
+            <p>Hébergeur :</p>
+            <p>Infomaniak</p>
+            <p>Rue Eugène-Marziano 25</p>
+            <p>1227 Les Acacias</p>
+            <p>SUISSE</p>
+            <p>CHE-103.167.648</p>
+        </div>
+
+        <div class="my-4">
+            <p>Conditions d’utilisation :</p>
+            <p>Conformément à la loi Informatique et Libertés du 6 janvier 1978, vous disposez d'un droit d'accès et de rectification
+            aux données vous concernant par simple demande mail, écrite ou téléphonique. Les étudiants de MMI ne peuvent être tenus
+            responsables du contenu du site. Ce site est exploité par "Nom de la structure". La présentation et chacun des éléments, y
+            compris les marques, logos et noms de domaine, apparaissant sur le site https://jpo-mmi.steib-audy.fr sont protégés par les
+            lois en vigueur sur la propriété intellectuelle, et appartiennent à "Nom de la structure", ou font l'objet d'une
+            autorisation d'utilisation. Aucun élément composant le Site ne peut être copié, reproduit, modifié, réédité, chargé,
+            dénaturé, transmis ou distribué de quelque manière que ce soit, sous quelque support que ce soit, de façon partielle ou
+            intégrale, sans l'autorisation écrite et préalable de "Nom de la structure" à l'exception d'une stricte utilisation pour
+            les besoins de la presse et sous réserve du respect des droits de propriété intellectuelle et de tout autre droit de
+            propriété dont il est fait mention. Seule la copie à usage privé est autorisée pour votre usage personnel, privé et
+            non-commercial, sur votre ordinateur personnel. Toute utilisation autorisée des éléments composant ou figurant dans le
+            site doit se faire sans dénaturation, modification ou altération de quelque manière que ce soit. "Nom de la structure" se
+            réserve le droit de poursuivre tout acte de contrefaçon de ses droits de propriété intellectuelle. Le présent Contrat
+            est soumis au droit Français. Le présent Contrat s'applique dans le monde entier, et seule sa version en langue
+            française fait foi en vue de son interprétation.</p>
+        </div>
+
+        <div class="my-4">
+            <p>Graphisme, photos et illustrations :</p>
+            <p>Le graphisme (incluant le webdesign et le logo) a été réalisé par Iris Lavaine. Les photographies
+                appartiennent au
+                département MMI, etc.
+            </p>
+            <p>Déclaration à la CNIL : Conformément à la loi 78-17 du 6 janvier 1978 (modifiée par la loi 2004-801 du 6
+                août 2004
+                relative à la protection des personnes physiques à l’égard des traitements de données à caractère
+                personnel) relative à
+                l’informatique, aux fichiers et aux libertés, ce site n’a pas fait l’objet d’une déclaration auprès de
+                la Commission
+                nationale de l’informatique et des libertés (www.cnil.fr).
+            </p>
+        </div>
+
+        <div class="my-4">
+            <p>Litiges :</p>
+            <p>Les présentes conditions du site https://jpo-mmi.steib-audy.fr sont régies par les lois françaises et toute contestation ou
+            litiges qui pourraient naître de l’interprétation ou de l’exécution de celles-ci seront de la compétence exclusive des
+            tribunaux dont dépend le siège social de la société. La langue de référence, pour le règlement de contentieux éventuels,
+            est le français.</p>
+        </div>
+    </div>
+</template>
