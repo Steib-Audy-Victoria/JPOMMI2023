beb27fb86060cc6025877895a22a5062c9b59906
Steib-Audy-Victoria <95017413+Steib-Audy-Victoria@users.noreply.github.com>
2023-01-14 19:00:18 +0100

    modification mention légales

--- src/views/MentionLegalView.vue
+++ src/views/MentionLegalView.vue
@@ -11,14 +11,7 @@
             préavis, nous
             vous engageons à les consulter régulièrement.</p>
 
-        <div class="my-4">
-            <p>Responsable éditorial et responsable légal :</p>
-            <p>"Nom de la structure"</p>
-            <p>Représenté(e) par :</p>
-            <p>Adresse :</p>
-            <p>E-mail :</p>
-            <p>Siret ou N°TVA :</p>
-        </div>
+
 
         <div class="my-4">
             <p>Éditeur du site :</p>
@@ -40,16 +33,16 @@
             <p>Conditions d’utilisation :</p>
             <p>Conformément à la loi Informatique et Libertés du 6 janvier 1978, vous disposez d'un droit d'accès et de rectification
             aux données vous concernant par simple demande mail, écrite ou téléphonique. Les étudiants de MMI ne peuvent être tenus
-            responsables du contenu du site. Ce site est exploité par "Nom de la structure". La présentation et chacun des éléments, y
+            responsables du contenu du site. Ce site est exploité par Département MMI. La présentation et chacun des éléments, y
             compris les marques, logos et noms de domaine, apparaissant sur le site https://jpo-mmi.steib-audy.fr sont protégés par les
-            lois en vigueur sur la propriété intellectuelle, et appartiennent à "Nom de la structure", ou font l'objet d'une
+            lois en vigueur sur la propriété intellectuelle, et appartiennent à Département MMI, ou font l'objet d'une
             autorisation d'utilisation. Aucun élément composant le Site ne peut être copié, reproduit, modifié, réédité, chargé,
             dénaturé, transmis ou distribué de quelque manière que ce soit, sous quelque support que ce soit, de façon partielle ou
-            intégrale, sans l'autorisation écrite et préalable de "Nom de la structure" à l'exception d'une stricte utilisation pour
+            intégrale, sans l'autorisation écrite et préalable de Département MMI à l'exception d'une stricte utilisation pour
             les besoins de la presse et sous réserve du respect des droits de propriété intellectuelle et de tout autre droit de
             propriété dont il est fait mention. Seule la copie à usage privé est autorisée pour votre usage personnel, privé et
             non-commercial, sur votre ordinateur personnel. Toute utilisation autorisée des éléments composant ou figurant dans le
-            site doit se faire sans dénaturation, modification ou altération de quelque manière que ce soit. "Nom de la structure" se
+            site doit se faire sans dénaturation, modification ou altération de quelque manière que ce soit. Département MMI se
             réserve le droit de poursuivre tout acte de contrefaçon de ses droits de propriété intellectuelle. Le présent Contrat
             est soumis au droit Français. Le présent Contrat s'applique dans le monde entier, et seule sa version en langue
             française fait foi en vue de son interprétation.</p>
